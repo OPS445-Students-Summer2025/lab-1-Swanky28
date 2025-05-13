eeda3c2a825b00b997c04ea094b0a136ca0c1d71
Anish K C <Ak-c7@myseneca.ca>
2025-05-13 09:19:05 -0400

    Completed Lab 1

---
+++ lab1a.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+'''This program will print Hello World'''
+
+print('Hello world')
+
+
---
+++ lab1d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''This script performs math'''
+
+x = 10
+
+y = 2
+
+z = 5
+
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(x + y * z))
+
+
